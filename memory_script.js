4fd25bc11ebcdc9fd0f36ed00350dfa256539801
Pamela <117684123+pame-scs@users.noreply.github.com>
2022-12-02 05:25:58 +0100

    Add files via upload

---
+++ memory_script.js
@@ -0,0 +1,11 @@
+var i;
+
+function change1(id) {
+    var x = "Images/"+id+"1.png";
+    document.getElementById(id).src = x;
+}
+function change2(id) {
+    var x = "Images/"+id+".png";
+    document.getElementById(id).src = x;
+    return
+}
